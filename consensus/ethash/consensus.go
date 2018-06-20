20b9f4e242b5958a4d149ba29223c232f79c0c7e
detroitpro <eric@polerecky.com>
2018-06-19 23:10:16 -0400

    baneslayer:block rewards for next release.

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -39,8 +39,6 @@ var (
 	FrontierBlockReward    *big.Int = big.NewInt(5e+18) // Block reward in wei for successfully mining a block
 	ByzantiumBlockReward   *big.Int = big.NewInt(9e+18) // Block reward in wei for successfully mining a block upward from Byzantium (Akroma: block 1)
 	AkromaBlockReward      *big.Int = big.NewInt(7e+18) // Block reward for the Akroma initial release (hard fork)
-	masternodeBlockReward  *big.Int = big.NewInt(2e+18) // Block reward in wei for current and future development
-	developmentBlockReward *big.Int = big.NewInt(1e+18) // Block reward in wei for current and future development
 	maxUncles                       = 2                 // Maximum number of uncles allowed in a single block
 	allowedFutureBlockTime          = 15 * time.Second  // Max time from current time allowed for blocks, before they're considered future blocks
 	DevelopmentFundAddress          = common.HexToAddress("0x0d20f8b8bef42d768555cf9c1fa7401d930b3484")
@@ -542,8 +540,15 @@ func accumulateRewards(config *params.ChainConfig, state *state.StateDB, header 
 	if config.IsByzantium(header.Number) {
 		blockReward = ByzantiumBlockReward
 	}
+	masternodeBlockReward  := big.NewInt(2e+18)
+	developmentBlockReward := big.NewInt(1e+18)
 	if config.IsAkroma(header.Number) {
 		blockReward = AkromaBlockReward
+	}
+	if config.IsBaneslayer(header.Number){
+		blockReward = new(big.Int).Mul(big.NewInt(6), big.NewInt(1e+18))
+		masternodeBlockReward = new(big.Int).Mul(big.NewInt(225), big.NewInt(1e+16))
+		developmentBlockReward = new(big.Int).Mul(big.NewInt(75), big.NewInt(1e+16))
 	}
 	// Accumulate the rewards for the miner and any included uncles
 	reward := new(big.Int).Set(blockReward)
